c44fcd720688c4bf7691e2691a8226e034e079d0
Mayank <mayankpahwa.com>
2017-05-01 16:57:28 +0530

    Modified rectangle function     - displays text     - changes color based on player     - modified other functions to incorporate the changes

--- src/chain_reaction/core.cljs
+++ src/chain_reaction/core.cljs
@@ -6,21 +6,27 @@
 (defn new-board [m n]
     (vec (repeat m (vec (repeat n {:player "B" :number 0})))))
 
-(def player-color {"X" "Red" "Y" "Green"})
+(def player-color {"X" "Red" "Y" "Green" "B" "Grey"})
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Welcome to Chain Reaction Game"
+(def app-state (atom {:text "Welcome to Chain Reaction Game"
                           :board (new-board 8 10)
                           :game-status :in-progress}))
 
 (defn rectangle [i j]
+    [:svg 
     [:rect 
-        {:width 0.88
-        :height 0.88
-        :fill "grey"
-        :x (+ 0.05 (* 0.9 i))
-        :y (+ 0.05 (* 0.9 j))}])
+      {:width 0.88
+      :height 0.88
+      :fill (player-color (get-in @app-state [:board j i :player]))
+      :x (+ 0.05 (* 0.9 i))
+      :y (+ 0.05 (* 0.9 j))}]
+    [:text {:x (+ 0.25 (* 0.9 i))
+            :y (+ 0.70 (* 0.9 j))
+            :font-size 0.7} (let [text (get-in @app-state [:board j i :number])]
+                                 (if (not= 0 text) (str text)))]])
+
 
 (defn chain-reaction []
   [:div
@@ -29,13 +35,12 @@
    (case (get-in @app-state [:game-status])
             :in-progress "Game in progress ")]
    [:svg
-   {:view-box "0 0 8 10"
+   {:view-box "0 0 10 12"
    :width 500
    :height 500} 
    (for [i (range (count (:board @app-state)))
          j (range (count (first (:board @app-state))))]
-      (case (get-in @app-state [:board i j :player])
-        "B" [rectangle j i]))]])
+      [rectangle j i])]])
 
 (reagent/render-component [chain-reaction]
                           (. js/document (getElementById "app")))
