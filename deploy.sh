df5974dec7ff31dad0590a1dd46fea649630e939
p4v4n <pavan.barcelona@gmail.com>
2017-05-11 15:21:16 +0530

    changed url of the git repo

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ cd resources/public
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "git@github.com:p4v4n/Chain-Reaction-Game.git" master:gh-pages
+git push --force --quiet "https://github.com:p4v4n/Chain-Reaction-Game.git" master:gh-pages
 rm -fr resources/public/.git
